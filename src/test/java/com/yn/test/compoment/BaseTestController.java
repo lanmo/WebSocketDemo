eca07ff08af12db7593d6e8e3493eb9c6ef37531
杨楠 <nanyang205380@sohu-inc.com>
2014-12-04 18:25:39 +0800

    增加单元测试

---
+++ src/test/java/com/yn/test/compoment/BaseTestController.java
@@ -0,0 +1,12 @@
+package com.yn.test.compoment;
+
+/**
+ * Copyright (C), nanyang205380@sohu-inc.com.
+ * @ClassName: BaseTestController   
+ * @Description: 测试基础类
+ * @author YangNan(杨楠)
+ * @date 2014年12月4日 下午6:24:44 
+ */
+public class BaseTestController {
+
+}
